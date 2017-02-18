c179cef8264529b008c8e988110ff0d9446ae417
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2017-02-18 22:48:03 +0200

    project created

---
+++ CatEmoji/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ketsu
+{
+    public class GameManager : MonoBehaviour
+    {
+        public float FadeInTime;
+
+        ScreenFaider Faider;
+
+        void Awake()
+        {
+
+        }
+
+        void Start()
+        {
+            Faider = ScreenFaider.instance;
+            Faider.SetTo(Color.black);
+            Faider.FadeOut(Color.black, FadeInTime, null);
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
