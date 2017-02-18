bbae8922fcfd869e536e36f8888e9faf6f83a107
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2017-02-18 23:08:05 +0200

    level editor

---
+++ CatEmoji/Assets/Scripts/Polygon.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Ketsu
+{
+    [RequireComponent(typeof(PolygonCollider2D))]
+    [RequireComponent(typeof(MeshFilter))]
+    [RequireComponent(typeof(MeshRenderer))]
+    [ExecuteInEditMode]
+    public class Polygon : MonoBehaviour
+    {
+        public Color Color;
+
+        [HideInInspector]
+        public PolygonCollider2D PC2D;
+
+        MeshFilter meshFilter;
+        MeshRenderer meshRenderer;
+
+        void Awake()
+        {
+            PC2D = GetComponent<PolygonCollider2D>();
+            meshFilter = GetComponent<MeshFilter>();
+            meshRenderer = GetComponent<MeshRenderer>();
+
+            meshRenderer.sharedMaterial.color = Color;
+
+            UpdateMeshFilter();
+        }
+
+#if UNITY_EDITOR
+        void Update()
+        {
+            if (Application.isPlaying || PC2D == null) return;
+
+            meshRenderer.sharedMaterial.color = Color;
+
+            UpdateMeshFilter();
+        }
+#endif
+
+        public void TransformTo(Polygon target, float time, Action callback)
+        {
+            StartCoroutine(AnimateTo(target, time, callback));
+        }
+
+        IEnumerator AnimateTo(Polygon target, float time, Action callback)
+        {
+            Vector3 startingPos = transform.position;
+            Vector2[] startingPoints = PC2D.points;
+
+            float timePassed = 0;
+            float progress = 0;
+            while (progress < 0.99999f)
+            {
+                timePassed += Time.deltaTime;
+                progress = Mathf.Min(timePassed / time, 1.0f);
+
+                Vector2[] newPoints = new Vector2[PC2D.points.Length];
+                for (int i = 0; i < PC2D.points.Length; ++i)
+                {
+                    transform.position = Vector3.Lerp(startingPos, target.transform.position, progress);
+                    newPoints[i] = Vector2.Lerp(startingPoints[i], target.PC2D.points[i], progress);
+                }
+                PC2D.points = newPoints;
+
+                meshRenderer.sharedMaterial.color = Color.Lerp(Color, target.Color, progress);
+
+                UpdateMeshFilter();
+
+                yield return null;
+            }
+
+            callback();
+        }
+
+        void UpdateMeshFilter()
+        {
+            int pointCount = PC2D.GetTotalPointCount();
+
+            Mesh mesh = new Mesh();
+            Vector2[] points = PC2D.points;
+            Vector3[] vertices = new Vector3[pointCount];
+            for (int j = 0; j < pointCount; j++)
+            {
+                Vector2 actual = points[j];
+                vertices[j] = new Vector3(actual.x, actual.y, 0);
+            }
+
+            Triangulator tr = new Triangulator(points);
+            int[] triangles = tr.Triangulate();
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+
+            meshFilter.mesh = mesh;
+        }
+    }
+}
---
+++ CatEmoji/Assets/Scripts/PolygonGroup.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ketsu
+{
+    public class PolygonGroup : MonoBehaviour
+    {
+        public PolygonGroup TargetGroup;
+        public float TransformTime;
+
+        Polygon[] polygons;
+
+        bool transforming;
+        int waitingForPolygons;
+
+        void Awake()
+        {
+            polygons = GetComponentsInChildren<Polygon>();
+        }
+
+        void Update()
+        {
+            if (!transforming && TargetGroup != null && Input.anyKeyDown)
+            {
+                StartCoroutine(TransformTo(TargetGroup));
+            }
+        }
+
+        IEnumerator TransformTo(PolygonGroup target)
+        {
+            transforming = true;
+            waitingForPolygons = polygons.Length;
+
+            for (int i = 0; i < polygons.Length; ++i)
+            {
+                polygons[i].TransformTo(target.polygons[i], TransformTime, OnPolygonReady);
+            }
+
+            while (waitingForPolygons > 0) yield return null;
+            transforming = false;
+        }
+
+        void OnPolygonReady()
+        {
+            waitingForPolygons--;
+        }
+    }
+}
+
